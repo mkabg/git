a2cc2bd3f754a1a2e369c318ab6e4c2ea363f99f
mkabg <m7110645k@gmail.com>
2025-05-14 12:24:19 +0300

    add branch

--- git and git hub/Program.cs
+++ git and git hub/Program.cs
@@ -11,6 +11,7 @@ namespace git_and_git_hub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello mordechai");
         }
     }
 }
